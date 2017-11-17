04aa136b5279a35d90a836ccd07109d76682b5a7
Cedric Sohrauer <sohrauer@gmail.com>
2017-11-17 15:50:38 +0100

    readd normalize.Version with basic test

---
+++ normalize/version.go
@@ -0,0 +1,9 @@
+package normalize
+
+import "github.com/remerge/go-tools/version"
+
+// Version returns a noramlized version string in the format major.minor.patch
+// if minor and patch are zero only the major verison is returned
+func Version(s string) string {
+	return version.Parse(s).String()
+}
---
+++ normalize/version_test.go
@@ -0,0 +1,13 @@
+package normalize
+
+import (
+	"testing"
+)
+
+func TestVersion(t *testing.T) {
+	// Basic Test. Logic is tested in version.Version#String
+	normalizedVersion := Version("android : 5.1.0 : L : sdk=21 : LOLLIPOP : sdk=21")
+	if normalizedVersion != "5.1" {
+		t.Errorf("Expected normalized version to '5.1' but it was '%s'", normalizedVersion)
+	}
+}
