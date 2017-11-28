9d15209bd264258eaed477b487ec6d062c685f5b
Cedric Sohrauer <sohrauer@gmail.com>
2017-11-28 10:39:04 +0100

    Add docs on what the method actually does

--- normalize/app_id.go
+++ normalize/app_id.go
@@ -8,6 +8,8 @@ import (
 var iosAppstoreRefRegexp = regexp.MustCompile(`^(?:id)[0-9]+$`)
 
 // AppId returns a normalized version of the app id.
+// At the moment, it only removes the "id" prefix from iOS App Ids
+// that AppsFyler adds.
 func AppId(ref string) string {
 	if iosAppstoreRefRegexp.MatchString(ref) {
 		return strings.TrimLeft(ref, "id")
